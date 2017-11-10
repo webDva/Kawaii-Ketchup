4f5faf5f630a913c89462e6874e72f6f184fb2f8
Web D.va <webNoDva@gmail.com>
2017-11-10 17:44:38 -0500

    A wee bit of refactoring.

--- public_html/typescript/game.ts
+++ public_html/typescript/game.ts
@@ -1,7 +1,7 @@
 /*
  * Template
  */
-module GameModuleName {
+module KetchupAndRaisins {
     /*
      * Boot state for only loading the loading screen
      */
@@ -37,6 +37,7 @@ module GameModuleName {
 
         preload() {
             // Display the loading screen image
+
             // Load assets
 
             this.game.load.image('player', 'assets/kawaii_chan.png');
@@ -58,28 +59,32 @@ module GameModuleName {
         }
     }
 
-    // enum for movement directions
+    // Enum for the controls.
     export enum Movement {
         Left,
         Right,
-        Jump,
+        Up,
         Down
     }
 
+    /*
+     * Derived KetchupSprite class for handling ketchup bottle properties.
+     */
     export class KetchupSprite extends Phaser.Sprite {
-        targetToFollow: Phaser.Sprite;
-        isFollowing: boolean = false;
+
+        targetToFollow: Phaser.Sprite; // Will be following the player sprite.
+        isFollowing: boolean = false; // Initially, it won't be following the player at its birth.
 
         constructor(game: Phaser.Game, x: number, y: number, key: string) {
             super(game, x, y, key);
 
             this.game.physics.arcade.enable(this);
-            this.body.gravity = new Phaser.Point(-this.game.physics.arcade.gravity.x, 400);
+            this.body.gravity = new Phaser.Point(-this.game.physics.arcade.gravity.x, PlayingState.GRAVITY_Y_VECTOR);
             this.checkWorldBounds = true;
             this.outOfBoundsKill = true;
-            this.anchor.setTo(0.5, 0.5);
+            this.anchor.setTo(0.5, 0.5); // Set the center of the ketchup bottles.
 
-            this.scale.setTo(1.5, 1.5);
+            this.scale.setTo(1.5, 1.5); // Increase the size of the ketchup bottles so that the player can more easily see them.
 
             // Add to the display, but the physics system already did this, so this is redundant.
             this.game.stage.addChild(this);
@@ -91,35 +96,49 @@ module GameModuleName {
                 this.body.velocity.x = Math.cos(angle) * 200;
                 this.body.velocity.y = Math.sin(angle) * 200;
 
-                this.rotation = angle + Math.PI / 2;
+                this.rotation = angle + Math.PI / 2; // Changing the rotation so that it can look better than if the rotation wasn't changed.
             }
         }
     }
     /*
      * The main game running state
      */
-    export class GameState extends Phaser.State {
+    export class PlayingState extends Phaser.State {
+
         game: Phaser.Game;
 
         player: Phaser.Sprite;
-        health: number = 100;
-        static INITIAL_HEALTH: number = 100;
+        currentHealth: number = PlayingState.INITIAL_HEALTH;
         score: number = 0;
         textScore: Phaser.Text;
 
-        missile: Phaser.Sprite;
         ketchupGroup: Phaser.Group;
         raisinGroup: Phaser.Group;
-
-        raisinsCollected: number = 0;
 
         lifeBar: Phaser.Graphics;
 
         // keyboard cursor key controls
         cursors: Phaser.CursorKeys;
 
-        static MOVE_VELOCITY: number = 365;
-        static JUMP_VELOCITY: number = GameState.MOVE_VELOCITY + GameState.MOVE_VELOCITY * 0.38;
+        // A bunch of constant values.
+
+        static MOVEMENT_SPEED: number = 365;
+        static JUMPING_SPEED: number = PlayingState.MOVEMENT_SPEED + PlayingState.MOVEMENT_SPEED * 0.38;
+        static GRAVITY_Y_VECTOR: number = 400;
+
+        static INITIAL_HEALTH: number = 100;
+
+        static KETCHUP_SPAWN_RATE: number = 800; // Number of milliseconds for spawning ketchup bottles.
+
+        static RAISIN_POINT_VALUE: number = 10; // How much collecting an individual raisin is worth.
+        static HEAL_AMOUNT: number = 1; // Determines how much to increase the player's health by when a raisin is collected.
+
+        static LIFEBAR_COLOR: number = 0xC70039;
+        static SCORE_TEXT_COLOR: string = "#2FDF00";
+
+        // Unused stuff can go here.
+
+        raisinsCollected: number = 0; // Can use this for calculating a score at the end of the game. Currently unused though.
 
         constructor() {
             super();
@@ -127,27 +146,29 @@ module GameModuleName {
 
         create() {
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
-            //this.game.physics.arcade.gravity.x = -400;            
 
             // add cursor keys controls
             this.cursors = this.game.input.keyboard.createCursorKeys();
 
+            // Add and configure the player sprite.
             this.player = this.game.add.sprite(100, this.game.world.centerY, 'player');
-            this.player.scale.setTo(2, 2);
+            this.player.scale.setTo(2, 2); // Increase the player's sprite size so that it can be more easily seen.
             this.game.physics.arcade.enable(this.player);
-            this.player.body.gravity = new Phaser.Point(-this.game.physics.arcade.gravity.x, 400);
+            this.player.body.gravity = new Phaser.Point(-this.game.physics.arcade.gravity.x, PlayingState.GRAVITY_Y_VECTOR);
             this.player.body.collideWorldBounds = true;
             this.player.anchor.setTo(0.5, 0.5);
 
+            // Create the Group that will hold the ketchup bottles.
             this.ketchupGroup = this.game.add.group();
 
+            // A spawn timer for creating ketchup bottles.
             let spawnTimer = this.game.time.create(false);
-            spawnTimer.loop(800, () => {
+            spawnTimer.loop(PlayingState.KETCHUP_SPAWN_RATE, () => {
                 let singleKetchup = this.ketchupGroup.add(
                     new KetchupSprite(this.game, this.game.rnd.integerInRange(0, this.game.width), this.game.rnd.integerInRange(0, 150), 'ketchup')
                 );
 
-                // wait then attack
+                // Wait, then attack the player.
 
                 let waitTimer = this.game.time.create(true);
                 waitTimer.add(500, () => {
@@ -167,10 +188,10 @@ module GameModuleName {
             }, this);
             spawnTimer.start();
 
-            this.textScore = this.game.add.text(0, 50, "" + this.raisinsCollected, {
+            this.textScore = this.game.add.text(0, 50, "", {
                 font: '4em "Segoe UI", Impact, sans-serif',
                 fontWeight: "700",
-                fill: "#2FDF00",
+                fill: PlayingState.SCORE_TEXT_COLOR,
                 align: "center"
             });
 
@@ -188,14 +209,11 @@ module GameModuleName {
             raisinSpawnTimer.start();
 
             this.lifeBar = this.game.add.graphics(10, 10);
-            this.lifeBar.beginFill(0xC70039);
-            this.lifeBar.drawRoundedRect(0, 0, (this.game.width - 20) * (this.health / GameState.INITIAL_HEALTH), 30, 9);
-            this.lifeBar.endFill();
-            this.lifeBar.beginFill(0x999999);
+            this.drawLifeBar();
 
             let dyingHealthTimer = this.game.time.create(false);
             dyingHealthTimer.loop(1800, () => {
-                this.health--;
+                this.currentHealth--;
             }, this);
             dyingHealthTimer.start();
 
@@ -209,24 +227,24 @@ module GameModuleName {
         /*
          * controls player horizontal movement
          */
-        movePlayer(direction: GameModuleName.Movement) {
+        movePlayer(direction: KetchupAndRaisins.Movement) {
             // If the player is in mid-air, decrease their movement speed by 10%.
             let speedModifier = 0;
             if (!this.player.body.onFloor()) {
-                speedModifier = 0.10 * GameState.MOVE_VELOCITY;
+                speedModifier = 0.10 * PlayingState.MOVEMENT_SPEED;
             }
 
-            if (direction === GameModuleName.Movement.Left) {
-                this.player.body.velocity.x = -GameState.MOVE_VELOCITY - speedModifier;
-            } else if (direction === GameModuleName.Movement.Right) {
-                this.player.body.velocity.x = GameState.MOVE_VELOCITY - speedModifier;
-            } else if (direction === GameModuleName.Movement.Jump) {
+            if (direction === KetchupAndRaisins.Movement.Left) {
+                this.player.body.velocity.x = -PlayingState.MOVEMENT_SPEED - speedModifier;
+            } else if (direction === KetchupAndRaisins.Movement.Right) {
+                this.player.body.velocity.x = PlayingState.MOVEMENT_SPEED - speedModifier;
+            } else if (direction === KetchupAndRaisins.Movement.Up) {
                 // checks to see if the player is on the ground, then jumps and plays jumping sound
                 if (this.player.body.onFloor()) {
-                    this.player.body.velocity.y = -GameState.JUMP_VELOCITY;
+                    this.player.body.velocity.y = -PlayingState.JUMPING_SPEED;
                 }
-            } else if (direction === GameModuleName.Movement.Down) {
-                this.player.body.velocity.y = GameState.MOVE_VELOCITY;
+            } else if (direction === KetchupAndRaisins.Movement.Down) {
+                this.player.body.velocity.y = PlayingState.MOVEMENT_SPEED;
             }
         }
 
@@ -241,17 +259,28 @@ module GameModuleName {
             }, this);
 
             ketchup.kill();
-            this.health--;
+            this.currentHealth--;
 
             this.game.camera.shake(0.01, 250);
         }
 
         collisionRaisinPlayer(player: Phaser.Sprite, raisin: Phaser.Sprite) {
-            raisin.kill();
-            this.score += 10;
-            if (this.health < GameState.INITIAL_HEALTH) {
-                this.health++;
+            raisin.kill(); // Remove the raisin when it's been collected.
+            this.score += PlayingState.RAISIN_POINT_VALUE; // Increment the score by a raisin point value.
+            if (this.currentHealth < PlayingState.INITIAL_HEALTH) { // Increase the player's health, but only if they already aren't at full health.
+                this.currentHealth += PlayingState.HEAL_AMOUNT;
             }
+        }
+
+        /*
+         * For drawing the player's health bar at the top of the screen.
+         */
+        drawLifeBar() {
+            this.lifeBar.clear();
+            this.lifeBar.beginFill(PlayingState.LIFEBAR_COLOR);
+            this.lifeBar.drawRoundedRect(0, 0, (this.game.width - 20) * (this.currentHealth / PlayingState.INITIAL_HEALTH), 30, 9);
+            this.lifeBar.endFill();
+            this.lifeBar.beginFill(0x999999); // Not sure why this is needed, really.        
         }
 
         update() {
@@ -265,22 +294,18 @@ module GameModuleName {
 
             // processing cursor keys or onscreen controls input to move the player avatar
             if (this.cursors.left.isDown) {
-                this.movePlayer(GameModuleName.Movement.Left);
+                this.movePlayer(KetchupAndRaisins.Movement.Left);
             }
             else if (this.cursors.right.isDown) {
-                this.movePlayer(GameModuleName.Movement.Right);
+                this.movePlayer(KetchupAndRaisins.Movement.Right);
             }
             if (this.cursors.up.isDown) {
-                this.movePlayer(GameModuleName.Movement.Jump);
+                this.movePlayer(KetchupAndRaisins.Movement.Up);
             } else if (this.cursors.down.isDown) {
-                this.movePlayer(GameModuleName.Movement.Down);
+                this.movePlayer(KetchupAndRaisins.Movement.Down);
             }
 
-            this.lifeBar.clear();
-            this.lifeBar.beginFill(0xC70039);
-            this.lifeBar.drawRoundedRect(0, 0, (this.game.width - 20) * (this.health / GameState.INITIAL_HEALTH), 30, 9);
-            this.lifeBar.endFill();
-            this.lifeBar.beginFill(0x999999);
+            this.drawLifeBar();
         }
     }
 
@@ -295,11 +320,11 @@ module GameModuleName {
              */
             this.game.state.add("BootState", BootState, true);
             this.game.state.add("PreloadState", PreloadState);
-            this.game.state.add("GameState", GameState);
+            this.game.state.add("GameState", PlayingState);
         }
     }
 }
 
 window.onload = () => {
-    let game = new GameModuleName.Game();
+    let game = new KetchupAndRaisins.Game();
 };
